94896d23d3cdf89df4fd69fb3aeb1b2f253ea077
aakash2809 <aakashrajak289@gmail.com>
2020-11-18 02:36:00 +0530

    start line comparison

---
+++ src/main/java/com/LineComparison/LineComparison.java
@@ -0,0 +1,12 @@
+package com.LineComparison;
+
+public class LineComparison {
+
+	// MAIN METHOD
+	public static void main(String[] args) {
+		// DISPLAYING THE MESSAGE
+		System.out.println(" Welcome to Line Comparison Computation Program ");
+
+	}
+
+}
